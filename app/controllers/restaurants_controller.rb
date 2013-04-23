5b8d6f6bfc3d8a7ac2b126db56d19c3db869cce6
Alex Gorski <alex.gorski@gmail.com>
2013-04-23 15:02:34 -0400

    pulling eric figaro

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,6 +3,20 @@ class RestaurantsController < ApplicationController
   def index
     if current_user
     @restaurants = Restaurant.all.sort
+    #remove any 2006 rankings so that a proper nymag link can be built
+
+    # something like 
+#     /(.*)\.(.*)/.match("what.to.do")[1..2]
+# => ["what.to", "do"]
+# or perhaps more conventionally,
+
+# s = "what.to.do"
+
+# s.match(/(.*)\.(.*)/)[1..2]
+# => ["what.to", "do"]
+# u = "wd~50 (4)"
+# u.match(/(.*)\((.*)/)[1..4] returns ["wd~50 ","4)"]
+    @restaurants.collect! {|r| r.nymag_page.match(/(.*)\((.*)/)[1..4][0]}
     else
       redirect_to new_user_path
     end
