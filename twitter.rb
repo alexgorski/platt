9f61380ed8f2d345a05ec89a4fce465a69b6b1f5
Eric Iacutone <eric.iacutone@gmail.com>
2013-04-30 13:42:00 -0400

    twitter search hardcoded

--- twitter.rb
+++ twitter.rb
@@ -2,13 +2,20 @@ require 'twitter'
 require 'pry'
 
 Twitter.configure do |config|
-  config.consumer_key = '3ulQawSASFuqM2JZsGioKA'
-  config.consumer_secret = '4Lc3kf3XqNLUcq5R4ZiuVEOQ6xlr5dh6dKfNBrBRlo'
-  config.oauth_token = '21169833-46EIWBHKh93RZ5qOGn0u3YPto9ajXJjt9rEQUebFk'
-  config.oauth_token_secret = 'zIkVKHa9GsAyIIqt2IVNnGIRXfbHaQEBYYvGHyq2tg'
+  config.consumer_key = ENV["TWITTER_KEY"]
+  config.consumer_secret = ENV["TWITTER_SECRET"]
+  config.oauth_token = ENV["TWITTER_OATH_TOKEN"]
+  config.oauth_token_secret = ENV["TWITTER_OATH_TOKEN_SECRET"]
 end
 
 
 @tweets = Twitter.search('Momofuku Ko')
-binding.pry
-puts @tweets
+@tweet = @tweets.to_hash
+@tweet.each do |t|
+	@total = []
+	@text = []
+	@total << t[1][0][:id]
+	@text << t[1][0][:text]
+end
+	
+end
